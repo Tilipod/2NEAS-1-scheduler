91aea79c8a5ee1b34b5d3f6237bc2c3c79a40032
Тилипкин Владислав Алексеевич <tilipkin.va@reliab.tech>
2022-04-04 11:35:46 +0300

    Init scheduler for 2NEAS-1

---
+++ src/main/java/ru/tilipod/NneasSchedulerApplication.java
@@ -0,0 +1,14 @@
+package ru.tilipod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NneasSchedulerApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(NneasSchedulerApplication.class, args);
+        }
+}
